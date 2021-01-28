f7aeb6a1e2bf5e7aaf2eb1943c085eccf4e1a1c5
Mounia Jaiti <47070088+moonjt1703@users.noreply.github.com>
2021-01-28 21:24:02 +0100

    Update SearchEngineForElements.py

--- SearchEngineForElements.py
+++ SearchEngineForElements.py
@@ -11,16 +11,16 @@ import sim_functions
 
 # Starting The preprocessing
 
-stemmer = PorterStemmer()
-stopwords = set(stopwords.words("english"))
+stemmer = PorterStemmer ()
+stopwords = set (stopwords.words ("english"))
 
 
 def doc_Preprocessing(values):
     myprocessedList = []
-    wnl = WordNetLemmatizer()
-    for value in values :
+    wnl = WordNetLemmatizer ()
+    for value in values:
         value = re.sub (r'\d+', '', value)
-        value = value.lower()
+        value = value.lower ()
         value = re.sub (r"\n", " ", value)
         value = value.lower ()
         translator = str.maketrans ('', '', string.punctuation)
@@ -37,39 +37,39 @@ def doc_Preprocessing(values):
 # Parsing TextFiles
 
 # list =[]
-my_texts_dictionnary = dict()
+my_texts_dictionnary = dict ()
 
 
 def parse_XMLFiles():
-    filenames = glob.glob('./Data/XML_file/coll/*.xml')
-    my_xmldict = dict()
+    filenames = glob.glob ('./Data/XML_file/coll/*.xml')
+    my_xmldict = dict ()
     for filename in filenames:
-        values=[]
-        processed_value=[]
-        mydoc = minidom.parse(filename)
-        items = mydoc.getElementsByTagName('bdy')
-        ids = mydoc.getElementsByTagName('id')
+        values = []
+        processed_value = []
+        mydoc = minidom.parse (filename)
+        items = mydoc.getElementsByTagName ('bdy')
+        ids = mydoc.getElementsByTagName ('id')
         for elem in items:
-            if elem.firstChild.data:
-                values.append(elem.firstChild.data)
-                processed_value.append(doc_Preprocessing(values))
-                for value in processed_value:
-                    key = ids[0].firstChild.data
-                    if value!=[]:
-                        my_xmldict.update({key:value})
-    print(my_xmldict)
-
+            for elem in elem.childNodes:
+                if elem.nodeType==elem.TEXT_NODE:
+                    values.append (elem.nodeValue)
+                    processed_value.append (doc_Preprocessing (values))
+        if processed_value != []:
+            for value in processed_value:
+                key = ids [0].firstChild.data
+                if value != []:
+                    my_xmldict.update ({key: value})
+    print (my_xmldict)
     return my_xmldict
 
 
-print(parse_XMLFiles())
+print (parse_XMLFiles ())
 
 
 def queryParser():
-    with open('Data/queries.txt') as f:
+    with open ('Data/queries.txt') as f:
         queries = []
-        lines = ''.join(f.readlines())
-        queries = [x.rstrip().split() for x in lines.split('\n')[:-1]]
+        lines = ''.join (f.readlines ())
+        queries = [x.rstrip ().split () for x in lines.split ('\n') [:-1]]
 
     return queries
-
