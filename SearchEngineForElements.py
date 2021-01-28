985b30362eea488dca474f6ed37de536337ad502
Mounia Jaiti <47070088+moonjt1703@users.noreply.github.com>
2021-01-28 20:11:33 +0100

    Update SearchEngineForElements.py

--- SearchEngineForElements.py
+++ SearchEngineForElements.py
@@ -18,19 +18,19 @@ stopwords = set(stopwords.words("english"))
 def doc_Preprocessing(values):
     myprocessedList = []
     wnl = WordNetLemmatizer()
-    values = re.sub(r'\d+', '', values)
-    values = values.lower()
-    values = re.sub(r"\n", " ", values)
-    values = values.lower()
-    translator = str.maketrans('', '', string.punctuation)
-    values = values.translate(translator)
-    tokensList = word_tokenize(values)
-    for token in tokensList:
-        foo = wnl.lemmatize(token)
-        foo = stemmer.stem(foo)
-        if foo not in stopwords:
-            myprocessedList.append(foo)
-    # print(myprocessedList)
+    for value in values :
+        value = re.sub (r'\d+', '', value)
+        value = value.lower()
+        value = re.sub (r"\n", " ", value)
+        value = value.lower ()
+        translator = str.maketrans ('', '', string.punctuation)
+        value = value.translate (translator)
+        tokensList = word_tokenize (value)
+        for token in tokensList:
+            foo = wnl.lemmatize (token)
+            foo = stemmer.stem (foo)
+            if foo not in stopwords:
+                myprocessedList.append (foo)
     return myprocessedList
 
 
@@ -40,37 +40,24 @@ def doc_Preprocessing(values):
 my_texts_dictionnary = dict()
 
 
-def parse_TextFiles():
-    with open('Data/Text_file/Text_Only_Ascii_Coll_MWI_NoSem', encoding="utf8") as file:
-        my_upgraded_textfile = ET.fromstringlist(["<documents>", str(file.read()), "</documents>"])
-        for i in range(len(my_upgraded_textfile)):
-            my_texts_dictionnary[int(my_upgraded_textfile[i][0].text)] = doc_Preprocessing(
-                my_upgraded_textfile[i][0].tail)
-            # print(my_texts_dictionnary.items())
-            return my_texts_dictionnary.items()
-
-
-# print(parse_TextFiles())
-
-
 def parse_XMLFiles():
     filenames = glob.glob('./Data/XML_file/coll/*.xml')
     my_xmldict = dict()
     for filename in filenames:
-
+        values=[]
+        processed_value=[]
         mydoc = minidom.parse(filename)
-        items = mydoc.getElementsByTagName('link')
+        items = mydoc.getElementsByTagName('bdy')
         ids = mydoc.getElementsByTagName('id')
         for elem in items:
             if elem.firstChild.data:
-                values = elem.firstChild.data
-                processed_value = doc_Preprocessing(values)
+                values.append(elem.firstChild.data)
+                processed_value.append(doc_Preprocessing(values))
                 for value in processed_value:
                     key = ids[0].firstChild.data
-                    if key not in my_xmldict:
-                        my_xmldict[key] = []
-                    my_xmldict[key].append(value)
-        print(my_xmldict)
+                    if value!=[]:
+                        my_xmldict.update({key:value})
+    print(my_xmldict)
 
     return my_xmldict
 
@@ -86,17 +73,3 @@ def queryParser():
 
     return queries
 
-
-#if __name__ == '__main__':
-   # print(queryParser())
-
-"""
-dictionarry = dict()
-
-
-def get_doc(doc_id):
-    dictionarry.get(doc_id)
-
-
-def get_dict_size(dictionarry):
-    return len(dictionarry)"""
